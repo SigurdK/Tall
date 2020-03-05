2e0c419fa91d60134c0244a653b54eb9ee3d054c
Sigurd Kalvik <Kalvik.sigurd@gmail.com>
2020-03-05 15:48:04 +0100

    la til leggTil()

---
+++ TallRekke.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class TallRekke {
+    ArrayList<Tall> tallrekke = new ArrayList<Tall>();
+    int antall;
+    int teller = 0;
+
+    public TallRekke(int a){
+        antall = a;
+
+    }
+
+    public void leggTil(Tall t){
+        if (tallrekke.size() <= antall)
+            tallrekke.add(t);
+        else
+            System.out.println("Ikke plass");
+    }
+
+
+
+}
